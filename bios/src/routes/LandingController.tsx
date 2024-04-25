96c9fe81c1b1c2dd10b63be50b2f7f8581792bde
selimtan <stanoncloud@gmail.com>
2024-04-25 12:47:21 +0300

    .

--- bios/src/routes/LandingController.tsx
+++ bios/src/routes/LandingController.tsx
@@ -1,9 +1,9 @@
 import { is } from "@tuval/core";
-import { DialogStack, ForEach, Fragment, HStack, ReactView, ScrollView, Text, UIController, UIImage, UINavigate, UIView, UIViewBuilder, VStack, cLeading, cTop, cTopLeading, cVertical, useDialogStack, useNavigate, useState } from "@tuval/forms";
+import { DialogStack, ForEach, Fragment, HStack, ReactView, ScrollView, Text, UIController, UIImage, UINavigate, UIView, UIViewBuilder, VStack, cLeading, cTop, cTopLeading, cVertical, useDialogStack, useEffect, useLocalStorage, useNavigate, useState } from "@tuval/forms";
 import { useGetSubdomain } from "../hooks/useGetSubdomain";
-import { GooleDriveBroker } from "@realmocean/sdk";
+import { GooleDriveBroker, useListGDriveFiles, useListGDriveFolders } from "@realmocean/sdk";
 import React from "react";
-import { FolderController, FolderView } from "../views/FolderView";
+import { FolderView } from "../views/FolderView";
 
 
 
@@ -12,10 +12,24 @@ import { FolderController, FolderView } from "../views/FolderView";
 export class LandingController extends UIController {
     public override LoadView(): UIView {
         const navigate = useNavigate();
-        const subDomain = useGetSubdomain();
 
-        const [folders, setFolders] = useState();
-        const [files, setFiles] = useState();
+        //const [folders, setFolders] = useState();
+        //const [files, setFiles] = useState();
+        const [token, setToken] = useLocalStorage('token', null);
+        const { folders, isLoading } = useListGDriveFolders('root', token);
+        const { files, isLoading: isFilesLoading } = useListGDriveFiles('root', token);
+
+        useEffect(() => {
+            /* if (token != null) {
+                GooleDriveBroker.Default.listFiles('root', token).then((files) => {
+                    setFiles(files);
+                });
+
+                GooleDriveBroker.Default.listFolders('root', token).then((folders) => {
+                    setFolders(folders);
+                });
+            } */
+        })
 
         return (
 
@@ -27,10 +41,16 @@ export class LandingController extends UIController {
                                 UIImage('/images/google_login.svg')
                             ).width().height()
                                 .onClick(async () => {
-                                    const files = await GooleDriveBroker.Default.listFiles();
-                                    const folders = await GooleDriveBroker.Default.listFolders();
+
+                                    /* const token = await GooleDriveBroker.Default.getUserToken();
+                                    GooleDriveBroker.Default.setToken(token);
+                                    setToken(token);
+
+
+                                    const files = await GooleDriveBroker.Default.listFiles('root', token);
+                                    const folders = await GooleDriveBroker.Default.listFolders('root', token);
                                     setFolders(folders);
-                                    setFiles(files);
+                                    setFiles(files); */
                                 }),
                             folders == null ? Fragment() :
                                 HStack({ alignment: cTopLeading })(
@@ -48,11 +68,13 @@ export class LandingController extends UIController {
                                                             .allHeight(50)
                                                             .borderBottom('solid 1px #CCCCCC55')
                                                             .background({ hover: '#F0F1F1' })
-                                                            .onClick(()=> {
+                                                            .onClick(() => {
                                                                 openDialog({
                                                                     title: 'Open',
-                                                                    view: FolderView(folder)
-                                                                })
+                                                                    view: UIViewBuilder(() =>
+                                                                        FolderView(folder)
+                                                                    )
+                                                                }) 
                                                             })
                                                     )
                                                 })
--- bios/src/views/FolderView.tsx
+++ bios/src/views/FolderView.tsx
@@ -1,96 +1,67 @@
-import { GooleDriveBroker } from "@realmocean/sdk";
-import { ForEach, HStack, Heading, ReactView, Text, UIController, UIImage, UIView, UIViewBuilder, VStack, cLeading, useDialogStack, useEffect, useState } from "@tuval/forms";
+import { GooleDriveBroker, useListGDriveFiles, useListGDriveFolders } from "@realmocean/sdk";
+import { ForEach, Fragment, HStack, Heading, ReactView, ScrollView, Text, UIController, UIImage, UIView, UIViewBuilder, VStack, cLeading, cTopLeading, cVertical, useDialogStack, useEffect, useState } from "@tuval/forms";
 import React from "react";
 
-export class FolderController extends UIController {
-    public LoadView(): UIView {
-        const { folderItem } = this.props;
-        const [files, setFiles] = useState();
 
-        useEffect(() => {
-            GooleDriveBroker.Default.listChild(folderItem.id).then((folders) => {
-                setFiles(folders)
-            })
-        }, []);
-        return (
+
+export const FolderView = (folderItem) => UIViewBuilder(() => {
+
+    const { folders, isLoading } = useListGDriveFolders(folderItem.id);
+    const { files, isLoading: isFilesLoading } = useListGDriveFiles(folderItem.id);
+
+    useEffect(() => {
+        /*    GooleDriveBroker.Default.listChild(folderItem.id).then((folders) => {
+               setFiles(folders)
+           }) */
+    }, []);
+    return (
+        (isLoading || isFilesLoading) ? Fragment() :
             VStack(
-                HStack(
+                HStack({ alignment: cLeading })(
+                    UIImage(folderItem.iconLink.replace('16', '32')),
                     Heading(folderItem.name)
                         .h2()
                 )
-                    .height(),
-                ...ForEach(files)((folder: any) =>
-                    UIViewBuilder(() => {
-                        const { openDialog } = useDialogStack();
-                        return (
-                            HStack({ alignment: cLeading, spacing: 5 })(
-                                UIImage(folder.iconLink.replace('16', '32')),
-                                Text(folder.name)
-                                    .foregroundColor('rgb(31, 31, 31)')
-                            )
-                                .allHeight(50)
-                                .borderBottom('solid 1px #CCCCCC55')
-                                .background({ hover: '#F0F1F1' })
-                                .onClick(() => {
-                                    openDialog({
-                                        title: 'Open',
-                                        view: FolderView(folder)
+                    .padding()
+                    .height(60),
+                folderItem.mimeType === 'application/vnd.google-apps.folder' ?
+                    VStack(
+                        ScrollView({ axes: cVertical, alignment: cTopLeading })(
+                            VStack(
+                                ...ForEach([...folders, ...files])((folder: any) =>
+                                    UIViewBuilder(() => {
+                                        const { openDialog } = useDialogStack();
+                                        return (
+                                            HStack({ alignment: cLeading, spacing: 5 })(
+                                                UIImage(folder.iconLink.replace('16', '32')),
+                                                Text(folder.name)
+                                                    .foregroundColor('rgb(31, 31, 31)')
+                                            )
+                                                .allHeight(50)
+                                                .borderBottom('solid 1px #CCCCCC55')
+                                                .background({ hover: '#F0F1F1' })
+                                                .onClick(() => {
+                                                    openDialog({
+                                                        title: folder.name,
+                                                        view: UIViewBuilder(() =>
+                                                            FolderView(folder)
+                                                        )
+                                                    })
+                                                })
+                                        )
                                     })
-                                })
-                        )
-                    })
 
-                )
-            )
-        )
-    }
-}
-
-export const FolderView = (folderItem) => UIViewBuilder(() => {
-    const [files, setFiles] = useState();
-    useEffect(() => {
-        GooleDriveBroker.Default.listChild(folderItem.id).then((folders) => {
-            setFiles(folders)
-        })
-    }, []);
-    return (
-        VStack(
-            HStack(
-                Heading(folderItem.mimeType)
-                    .h2()
-            )
-                .height(),
-            folderItem.mimeType === 'application/vnd.google-apps.folder' ?
-                VStack(
-                    ...ForEach(files)((folder: any) =>
-                        UIViewBuilder(() => {
-                            const { openDialog } = useDialogStack();
-                            return (
-                                HStack({ alignment: cLeading, spacing: 5 })(
-                                    UIImage(folder.iconLink.replace('16', '32')),
-                                    Text(folder.name)
-                                        .foregroundColor('rgb(31, 31, 31)')
                                 )
-                                    .allHeight(50)
-                                    .borderBottom('solid 1px #CCCCCC55')
-                                    .background({ hover: '#F0F1F1' })
-                                    .onClick(() => {
-                                        openDialog({
-                                            title: 'Open',
-                                            view: FolderView(folder)
-                                        })
-                                    })
-                            )
-                        })
-
+                            ).height()
+                        )
                     )
-                )
-                :
-                ReactView(
-                    <iframe src={`${folderItem.webViewLink}`} style={{ width: '100%', height: '100%', border:'none' }}></iframe>
-                )
+                        .padding()
+                    :
+                    ReactView(
+                        <iframe src={`${folderItem.webViewLink.replace('/view?', '/preview?')}`} style={{ width: '100%', height: '100%', border: 'none' }}></iframe>
+                    )
 
-        )
+            )
     )
 })
 
